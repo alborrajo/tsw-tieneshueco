c9935188d28fdc3177f1fa6da60d282f79767a1f
ismavazquezdev <40865072+ismavazquezdev@users.noreply.github.com>
2018-10-06 12:55:21 +0200

    Add files via upload

--- Controllers/encuesta/encuesta-controller.php
+++ Controllers/encuesta/encuesta-controller.php
@@ -1,11 +1,13 @@
 <?php
-include_once "../../Views/header-view.php";
-include_once "../../Views/header-view.php";
+include_once "Views/header-view.php";
+include_once "Views/footer-view.php";
 
-include_once "../../Views/encuestas/participar-encuesta-view.php";
-include_once "../../Views/encuestas/editar-encuesta-view.php";
+include_once "Views/encuestas/participar-encuesta-view.php";
+include_once "Views/encuestas/editar-encuesta-view.php";
 
-include_once "../../Classes/Encuesta.php";
+include_once "Classes/Encuesta.php";
+include_once "Classes/Fecha.php";
+include_once "Classes/Hora.php";
 
 class EncuestaController
 {
@@ -16,27 +18,73 @@ class EncuestaController
 
 			switch($_POST["action"])
 			{
-				case "participar":
-					//TODO: Anhadir o borrar una tupla de la tabla que relaciona usuarios y votos.
+
+				case "addVoto":
+
+				if($_POST["idEncuesta"] && $_POST["fecha"] && $_POST["hora"])
+				{
+					//Anhadir un voto a esa encuesta con el usuario de la sesion 
+
+				}
+				header("Location: index.php?controller=encuesta&action=participarencuesta");
+
+				break;
+
+				case "delVoto":
+
+				if($_POST["idEncuesta"] && $_POST["fecha"] && $_POST["hora"])
+				{
+					//Borrar un voto a esa encuesta con el usuario de la sesion 
+					
+				}
+
+				header("Location: index.php?controller=encuesta&action=participarencuesta");
+
 				break;
 
 				case "addFecha":
 					//TODO: Anhadir a la tabla fechas una nueva tupla con el id de la encuesta y la fecha.
+				if($_POST["idEncuesta"] && $_POST["fecha"])
+				{
+
+				}
+				header("Location: index.php?controller=encuesta&action=editencuesta");
 
 				break;
 
 				case "addHora":
 					//TODO: Anhadir a la tabla horas una nueva tupla con el id de la encuesta, la fecha y la hora de inicio y fin.
+				if($_POST["idEncuesta"] && $_POST["fecha"] && $_POST["hora"])
+				{
 
-				break;
-
-				case "delHora":
-					//TODO: Eliminar de la tabla horas la tupla que se corresponda con la encuesta, fecha y hora seleccionadas.
+				}
+				header("Location: index.php?controller=encuesta&action=editencuesta");
 
 				break;
 
 				case "delFecha":
 					//TODO: Eliminar de la tabla fecha la tupla que se corresponda con la encuesta y la fecha seleccionadas.
+				if($_POST["idEncuesta"] && $_POST["fecha"])
+				{
+
+				}
+				header("Location: index.php?controller=encuesta&action=editencuesta");
+
+				break;
+
+				case "delHora":
+					//TODO: Eliminar de la tabla horas la tupla que se corresponda con la encuesta, fecha y hora seleccionadas.
+					//TODO: Anhadir a la tabla horas una nueva tupla con el id de la encuesta, la fecha y la hora de inicio y fin.
+				if($_POST["idEncuesta"] && $_POST["fecha"] && $_POST["hora"])
+				{
+
+				}
+				header("Location: index.php?controller=encuesta&action=editencuesta");
+
+				break;
+
+				default:
+				header("Location: index.php");
 
 				break;
 			}
@@ -45,21 +93,29 @@ class EncuestaController
 		{
 			switch($_GET["action"])
 			{
-				case "edit":
+				case "editencuesta":
 					//TODO: Obtener la encuesta en la BD por medio de su link.
 					//Mostrar la vista edit de esa encuesta
 					break;
 
-				case "participar":
+				case "participarencuesta":
 					//TODO: Obtener la encuesta en la BD por medio de su link.
 					//Mostrar la vista participar para esa encuesta.
+					break;
+
+				default:
+				header("Location: index.php");
 					break;
 			}
 		}
 		else
 		{
-			$encuesta = new Encuesta("tieneshueco.com/enlace1284u28o3uroweh", "Pachanga brava", "Manolo el del bombo", Array("Sabado 15 agosto", "Domingo 16 agosto") );
+			
+			$horas = Array(new Hora("10:45","12:45"),new Hora("10:45","12:45"),new Hora( "10:45","12:45"));
 
+			$fechas = Array(new Fecha("Sabador 15 agosto", $horas), new Fecha("Viernes 14 agosto", $horas),new Fecha("Lunes 20 agosto", $horas));
+
+			$encuesta = new Encuesta("tieneshueco.com/enlace1284u28o3uroweh", "Pachanga brava", "Manolo el del bombo", $fechas);
 			new HeaderView(true);
 			new ParticiparView($encuesta);
 			new FooterView(true);
