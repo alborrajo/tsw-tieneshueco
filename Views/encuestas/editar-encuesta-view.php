6669716167a2d1a3d8c51e52c12e8f9220d6e9c4
ismavazquezdev <40865072+ismavazquezdev@users.noreply.github.com>
2018-10-09 10:30:19 +0200

    Add files via upload

--- Views/encuestas/editar-encuesta-view.php
+++ Views/encuestas/editar-encuesta-view.php
@@ -1,14 +1,29 @@
 <?php
 include_once "Classes/Encuesta.php";
+include_once "Classes/Fecha.php";
+include_once "Classes/Hora.php";
+include_once "Classes/Voto.php";
 
+include_once "Views/plantilla-view.php";
+?>
 
-class EditarView {
+<?php
+class EditarView extends PlantillaView{
 
 	private $encuesta;
+	private $votos;
 	private $msg;
 
-	function __construct($encuesta, $msg=NULL)
+	function __construct($encuesta,$votos, $msg=NULL)
 	{
+		$this->encuesta = $encuesta;
+		$this->votos = $votos;
+	}
+	
+
+	function _render()
+	{
+
 		?>
 
 		<main class= "container">
@@ -17,64 +32,89 @@ class EditarView {
 			<br/>
 			<em> Edita tu encuesta. </em>
 			</div>
-
-			<form action="/" method="POST" name="formularioencuesta">
-
-				<input type="hidden" name="action" value="edit">
-
-				<div id="encuesta" class="row">
-					<table class="table-borderer col-10">
-
-						<tr>
-							<th>
-
-							</th>
-						<?php
-						$fechas = $encuesta->fechas;
-						foreach($fechas as $fecha)
-						{
-							?>
-							<td>
-								<input type="button" value="Eliminar columna">
-							</td>
-							<?php
-						}
-						?>
-						</tr>
-
-						<tr>
-							<th>
-
-							</th>
-						<?php
-						$fechas = $encuesta->fechas;
-						foreach($fechas as $fecha)
-						{
-							?>
-							<td>
-								<?php echo $fecha->fechas;?>
-							</td>
-							<?php
-						}
-						?>
-						</tr>
-					</table>
-
-					<input type="button" class="col-2" value="Nueva columna">
-				</div>
-
 				<div class="linkencuesta">
 
 					<p> Este es el link de tu encuesta: </p>
 					<input type="text" readonly="readonly" size="50" value=<?php echo $encuesta->getID()?> >
 					<p> Compártelo para que la gente empiece a votar.</p>
+				</div>
 
 		<?php if($msg != null) { //Mostrar alerta si la variable $msg está establecida ?>
             <div class="alert" role="alert">
                 <?php echo $msg; ?>
             </div>       
         <?php } ?>
-        </main>
-        <?php
-    }
-}?>
+		</main>
+
+
+		<?php
+		
+		}
+	function subgruposVotos($votos)
+	{
+		//Creamos subgrupos con los votos de cada usuario
+			$usuarioActual = $votos[0]->getUsuario();
+			$votosAgrupados=Array();
+			$i = 0;
+			$k = 0;
+
+			foreach($votos as $voto)
+			{
+				if($voto->getUsuario()==$usuarioActual)
+				{
+					$votosAgrupados[$i][$k]=$voto;
+					$k++;
+				}
+				else
+				{
+					$k=0;
+					$i++;
+					$votosAgrupados[$i][$k]=$voto;
+				}
+			}
+			return $votosAgrupados;
+	}
+
+	function ordenarSubgrupos($subgrupo, $fechas)
+	{
+		$subgrupoOrdenado = Array();
+		for($i=0;$i<count($subgrupo);$i++)
+		{
+			array_push($subgrupoOrdenado, $subgrupo[0]);
+		}
+
+		$j=0;
+
+		foreach($fechas as $fecha)
+		{
+			$fechaActual=$fecha->getFecha();
+
+			for($i=0;$i<count($subgrupo);$i++)
+			{
+				if($fechaActual==$subgrupo[$i]->getFecha())
+				{
+					$subgrupoOrdenado[$j]=$subgrupo[$i];
+					$j++;
+				}
+			}
+		}
+		return $subgrupoOrdenado;
+	}
+
+	function comprobarVoto($fecha, $hora, $votos)
+	{
+		$toret=false;
+
+		foreach($votos as $voto)
+		{
+			if($fecha->getFecha()==$voto->getFecha() && $hora->getHoraInicio()==$voto->getHoraInicio()
+				&& $hora->getHoraFin()==$voto->getHoraFin())
+			{
+				$toret = true;
+			}
+		}
+		return $toret;
+
+	}
+}
+?>
