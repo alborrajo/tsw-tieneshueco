52546badeffcc50832bd8c362338bb2cf1f93fc6
Ismael Vazquez Fernandez <ismavazquezdev@gmail.com>
2018-10-10 17:24:08 +0200

    Formulario fecha

---
+++ Models/perfil/encuesta-model.php
@@ -0,0 +1,21 @@
+<?php
+
+include_once "Classes/MSGException.php";
+
+class EncuestaModel {
+
+    private $dbh;
+
+    function __construct() {
+        try {
+            $this->dbh = new PDO('mysql:host=localhost;dbname=tieneshueco', "tieneshueco", "tieneshueco");
+            $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $e) {
+            //Si no se hace así, se mostrarían todos los datos de la conexión, INCLUYENDO USER Y PASS DE LA BD
+            throw new MSGException("Error conectando con la BD","danger");
+        }
+    }
+}
+    
+?>
